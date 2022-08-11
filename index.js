a79c5e5d68e1fb6dff4e3f029b6cfdb7e1cb31c2
Ujjwal Garg <74059664+BRUTAlVision@users.noreply.github.com>
2022-08-11 22:57:15 +0530

    Adding Ajax part

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var xmlhttp = new XMLHttpRequest();
 const url = "https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
 
